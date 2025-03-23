254e2b142b6641a15ea2dc919274ebac608ba249
josue_dev <josuesa220617@gmail.com>
2025-03-23 20:31:22 -0300

    Criação layout de lista de cliente

--- front/loja_brinquedos/src/App.jsx
+++ front/loja_brinquedos/src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import PokemonList from './PokemonList';
+import Inicio from './Inicio';
 
 const App = () => {
  return (
   <Router>
    <Routes>
-    <Route path="/" element={<PokemonList />} />
+    <Route path="/" element={< Inicio/>} />
+    <Route path="/cliente" element={< PokemonList/>} />
    </Routes>
   </Router>
  );
---
+++ front/loja_brinquedos/src/componentes/Pesquisar.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Container, Box, TextField } from "@mui/material";
+
+export default function Pesquisar({ setSearchTerm }) {
+    const [searchTerm, setLocalSearchTerm] = useState(""); // Estado local para o termo de pesquisa
+
+    // Atualizando o estado global de pesquisa ao mudar o termo local
+    const handleSearchChange = (e) => {
+        const value = e.target.value;
+        setLocalSearchTerm(value);
+        setSearchTerm(value); // Atualiza o estado no componente Pai
+    };
+
+    return (
+        <Container maxWidth="lg" sx={{ padding: "40px 0" }}>
+            {/* Barra de pesquisa e botão */}
+            <Box sx={{
+                display: "flex",
+                flexDirection: { xs: "column", sm: "row" },
+                alignItems: "center",
+                justifyContent: "space-between",
+                marginLeft: '60px'
+               
+            }}>
+                <TextField
+                    type="text"
+                    placeholder="Busque por um cliente..."
+                    value={searchTerm}
+                    onChange={handleSearchChange} // Usando a função de mudança
+                    sx={{
+                        flex: 1,
+                        width: "200%",
+                        maxWidth: "800px",
+                        borderRadius: "25px",
+                        backgroundColor: "#fff",
+                        boxShadow: "0px 3px 6px rgba(0,0,0,0.1)",
+                        transition: "0.3s",
+                        "& .MuiOutlinedInput-root": {
+                            borderRadius: "25px",
+                            "&:hover fieldset": { borderColor: "#ff6f61" },
+                            "&.Mui-focused fieldset": { borderColor: "#d32f2f", borderWidth: "2px" },
+                        },
+                    }}
+                />
+            </Box>
+        </Container>
+    );
+}
